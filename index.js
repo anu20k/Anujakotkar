dbceaec9dd0906bbdead0ee62590da0f4cbe5bbe
anu20k <103042185+anu20k@users.noreply.github.com>
2022-12-15 23:42:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function message(){
+    confirm("Comform your form")
+}
+
+function windowlin(){
+    window.open("https://www.linkedin.com/in/anuja-kotkar-459416212")
+}
+
+function windowinsta(){
+    window.open("https://www.instagram.com/")
+}
+
+function windowfac(){
+    window.open("https://www.facebook.com/")
+}
+function windowtwi(){
+    window.open("https://twitter.com/home")
+}
+function windowgit(){
+    window.open("https://github.com/anu20k")
+}
